6b3e819281258ab1dc43ced571dfc5127f3ff9fb
heybluguy <heybluguy@yahoo.com>
2018-02-16 09:20:55 -0700

    Initial commit.

--- spec/features/user_can_see_an_individual_vending_machine_spec.rb
+++ spec/features/user_can_see_an_individual_vending_machine_spec.rb
@@ -4,9 +4,11 @@ feature 'When a user visits a vending machine show page' do
   scenario 'they see the location of that machine' do
     owner = Owner.create(name: "Sam's Snacks")
     dons  = owner.machines.create(location: "Don's Mixed Drinks")
+    snack = Snack.create(name: "Twix", price: 1, machine: dons)
 
     visit machine_path(dons)
 
     expect(page).to have_content("Don's Mixed Drinks Vending Machine")
+    expect(page).to have_content("Twix")
   end
 end
